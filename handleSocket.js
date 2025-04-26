7996de29dd847fdb8630e3c913de6beaceff7753
Devendra Nishad <ndevendra642@gmail.com>
2025-04-26 06:49:31 +0530

    Added Session Exchange

--- handleSocket.js
+++ handleSocket.js
@@ -1,11 +1,26 @@
 
+
            
             const handleSocket=(getAddress,socket,io)=>{
-              io.on('set-id',({id,partner_code})=>{
-                const address=getAddress(partner_code)
-                console.log(address)
-                socket.to(address).emit('get-id',id)
+
+              // Exchanging socket id with the help of pairing code
+
+              io.on('set-id',(code)=>{
+                const address=getAddress(code)
+              
+                socket.to(address).emit('get-id',io.id)
+                socket.to(io.id).emit('get-id',address)
               })
+              // session exchange 
+              io.on('session',({id,data})=>{
+                socket.to(id).emit('session',data)
+              })
+              // editor value exchange
+              io.on('change',({id,data})=>{
+                socket.to(id).emit('change',data)
+              })
+
+              
               
             
               
